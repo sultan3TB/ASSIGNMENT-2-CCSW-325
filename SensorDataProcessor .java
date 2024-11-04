8a1640ca939b05ec1d432a1110e6fe07cc7d2b36
MuathAlhassni <peef3898@gmail.com>
2024-11-04 11:18:59 +0300

    Update SensorDataProcessor .java

--- SensorDataProcessor .java
+++ SensorDataProcessor .java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 public class SensorDataProcessor { 
  
     // Senson data and limits. 
@@ -5,9 +8,9 @@ public class SensorDataProcessor {
     public double[][] limit; 
  
     // constructor 
-    public DataProcessor(double[][][] data, double[][] limit) { 
-        this.data = data; 
-        this.limit = limit; 
+   public SensorDataProcessor(double[][][] data, double[][] limit) {
+        this.data = data;
+        this.limit = limit;
     } 
  
     // calculates average of sensor data 
